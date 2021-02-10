c44eaf95882fd0d3c472b713e9e3ac701a843c91
wasefshaik@gmail.com <wasefshaik@gmail.com>
2021-02-10 17:06:36 +0530

    lap table added , needs upgrade

--- js/script.js
+++ js/script.js
@@ -3,11 +3,14 @@ var timer = document.getElementById("timer");
 var start = document.getElementById("start");
 var lap = document.getElementById("lap");
 var reset = document.getElementById("reset");
+var timer_box = document.getElementById("timer_box");
+
 
 //Set inital counters for time
 var seconds = 0 ;
 var minutes = 0;
 var hours = 0 ;
+var time = "00:00:00";
 
 //text to display in each block
 start.innerHTML = "START";
@@ -15,7 +18,7 @@ lap.innerHTML = "LAP";
 reset.innerHTML = "RESET";
 
 //inital timer display
-timer.innerHTML = "00:00:00";
+timer.innerHTML = time;
 
 //event listener for start button - triggers clock with stopper function
 start.addEventListener("click",stopper);
@@ -24,22 +27,24 @@ start.addEventListener("click",stopper);
 
 //state of the start button after clicking start
 function start_state(){
-	start.innerHTML = "START";
+	start.innerHTML = "START"; 
 	start.style.backgroundColor = "green";
 	reset.style.cursor = "pointer";
-	reset.addEventListener("click",resetter);
+	reset.addEventListener("click",resetter); // Now reset can be clicked to reset timer
 }
 
+// state of the start button after clicking stop
 function stop_state(){
 	start.innerHTML = "STOP";
 	start.style.backgroundColor = "RED";
 	reset.style.cursor = "not-allowed";
-	reset.removeEventListener("click",null);
+	reset.removeEventListener("click",null); // click event on reset is removed
 }
 
 //count clicks on start button
 var start_click = 0;
 var time_element ;
+
 
 function stopper(){
 	start_click += 1;
@@ -61,9 +66,9 @@ function resetter(){
 }
 
 
-
+// function called to start timer
 function start_timer(){
-	var seconds_display , hours_display , minutes_display , time;
+	var seconds_display , hours_display , minutes_display;
 
 	seconds += 1 ;
 
@@ -90,4 +95,30 @@ function start_timer(){
 	time = hours_display + ":" + minutes_display +":"+ seconds_display ; 
 
 	timer.innerHTML = time;
+}
+
+
+var scroll_lap = document.createElement("div");
+timer_box.append(scroll_lap);
+scroll_lap.style.width = "400px";
+scroll_lap.style.height = "400px";
+scroll_lap.style.border = "2px solid green";
+scroll_lap.style.position = "absolute";
+scroll_lap.style.top = "300px";
+scroll_lap.style.margin = "auto";
+scroll_lap.style.display = "flex";
+scroll_lap.style.overflow = "auto";
+scroll_lap.style.flexDirection = "column-reverse";
+
+lap.addEventListener("click",lap_list);
+
+function lap_list(){
+	var list = document.createElement("div");
+	scroll_lap.append(list);
+	list.style.width = "inherit";
+	list.style.height = "150px" ;
+	list.style.border = " 3px transparent";
+	list.style.borderBottom = "3px solid black";
+	list.style.display = ""
+	list.innerHTML = time ;
 }
